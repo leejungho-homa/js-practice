bfae8232e33af2e8246ae07966086bd253b3b3b3
leejungho-homa <leejho1107@naver.com>
2023-01-07 17:57:59 +0900

    js-practice
    2023.01.04

--- js/app.js
+++ js/app.js
@@ -2,26 +2,38 @@
 // if (isNaN(age)) {
 //   console.log("Please wirte a number");
 // } else {
-// }
+//
+
+// 이름 박스
 const loginform = document.querySelector(".form");
 const logininput = document.querySelector(".form input");
 const greetingbox = document.querySelector(".form-text");
 const greeting = document.querySelector(".form-text__text");
-const box1 = document.querySelector(".box1");
-const span = document.querySelector(".box1 span");
 const HIDDEN = "hidden";
+const username = logininput.value;
+const USERNAME = "username";
+const savedusername = localStorage.getItem(USERNAME);
 
-function clicksubmit(event) {
-  event.preventDefault();
+function clicksubmit() {
+  localStorage.setItem(USERNAME, logininput.value);
+  greeting.innerHTML = `Hello ${savedusername}`;
   loginform.classList.add(HIDDEN);
-  const username = logininput.value;
-  greeting.innerHTML = `Hello ${username}`;
   greetingbox.classList.remove(HIDDEN);
 }
+
+if (savedusername === null) {
+  loginform.classList.remove(HIDDEN);
+  loginform.addEventListener("submit", clicksubmit);
+} else {
+  greetingbox.classList.remove(HIDDEN);
+  greeting.innerHTML = `Hello ${savedusername}`;
+}
+// 박스 핸들링
+const box1 = document.querySelector(".box1");
+const span = document.querySelector(".box1 span");
 
 function handleboxclick() {
   box1.classList.toggle("box__active");
 }
 
 box1.addEventListener("click", handleboxclick);
-loginform.addEventListener("submit", clicksubmit);
